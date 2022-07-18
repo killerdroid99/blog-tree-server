c459b1bda5b38994dbd21506399b4e966a19f68a
killerdroid99 <ajempire09@gmail.com>
2022-07-18 20:48:44 +0530

    Basic crud

--- index.ts
+++ index.ts
@@ -1,24 +1,80 @@
 import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
+import express from "express";
+import bodyParser from "body-parser";
 
 const prisma = new PrismaClient();
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 async function main() {
-	// await prisma.user.create({
-	// 	data: {
-	// 		name: "Alice",
-	// 		email: "alice@prisma.io",
-	// 		password: "randompassword",
-	// 		posts: {
-	// 			create: {
-	// 				title: "What is Lorem Ipsum?",
-	// 				content:
-	// 					"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-	// 			},
-	// 		},
-	// 	},
-	// });
-	const allUsers = await prisma.user.findMany();
-	console.log(allUsers);
+	app.get("/", (req, res) => {
+		res.json({ msg: "hello world" });
+	});
+
+	// get all users
+	app.get("/users", async (req, res) => {
+		try {
+			const users: object[] = await prisma.user.findMany();
+			res.json({
+				msg: "got all users",
+				data: users,
+			});
+		} catch (error) {
+			res.status(400).json({ msg: "there was some error", error });
+		}
+	});
+
+	// create a new user
+	app.post("/users", async (req, res) => {
+		try {
+			const newuser: object = await prisma.user.create({
+				data: { ...req.body },
+			});
+			res.json({
+				msg: "user added successfully",
+				data: newuser,
+			});
+		} catch (error) {
+			res.status(400).json({ msg: "there was some error", error });
+		}
+	});
+
+	// update an existing user
+	app.patch("/users/:id", async (req, res) => {
+		try {
+			const updateduser: object = await prisma.user.update({
+				where: { id: req.params.id },
+				data: { ...req.body },
+			});
+			res.json({
+				msg: "user updated successfully",
+				data: updateduser,
+			});
+		} catch (error) {
+			res.status(400).json({ msg: "there was some error", error });
+		}
+	});
+
+	// deletes a user
+	app.delete("/users/:id", async (req, res) => {
+		try {
+			const deleteduser: object = await prisma.user.delete({
+				where: { id: req.params.id },
+			});
+			res.json({
+				msg: "user deleted successfully",
+				data: deleteduser,
+			});
+		} catch (error) {
+			res.status(400).json({ msg: "there was some error", error });
+		}
+	});
+
+	app.listen(process.env.PORT || 4000, () =>
+		console.log(`listening on http://localhost:${process.env.PORT || 4000}`)
+	);
 }
 
 main()
