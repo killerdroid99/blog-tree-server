12a1fbc2f5c9a23a4e51bab179b914b20171b948
killerdroid99 <ajempire09@gmail.com>
2023-09-07 09:51:02 +0530

    strict

--- index.ts
+++ index.ts
@@ -30,7 +30,7 @@ app.use(
 		cookie: {
 			maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // setting max age for cookie = 10 years
 			httpOnly: true,
-			sameSite: "none",
+			sameSite: "strict",
 			secure: process.env.PROD === "true" ? true : false,
 			path: "/",
 			domain: process.env.PROD === "true" ? process.env.CLIENT_URL : undefined,
